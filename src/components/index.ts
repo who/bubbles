cf9498d46ca08fc1c8331123548894820cac82cc
who <githubwho@gmail.com>
2026-05-06 23:00:40 -0700

    feat: ViewToggle Contract/Ticker segmented control (bubbles-702.7)

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,5 @@ export { default as FileDropzone, FILE_REJECT_MESSAGE } from './FileDropzone.tsx
 export type { FileDropzoneProps } from './FileDropzone.tsx';
 export { default as StatsStrip } from './StatsStrip.tsx';
 export type { StatsStripProps } from './StatsStrip.tsx';
+export { default as ViewToggle } from './ViewToggle.tsx';
+export type { ViewToggleProps, ViewMode, ViewToggleCounts } from './ViewToggle.tsx';
